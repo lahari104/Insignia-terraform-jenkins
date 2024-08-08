4aeb7511255a5d21b19a39cc9532849da396787d
lahari104 <113594229+lahari104@users.noreply.github.com>
2024-08-08 15:35:52 +0530

    Update backends.tf

--- terraform-jenkins/backends.tf
+++ terraform-jenkins/backends.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  backend "s3" {
+    bucket         = "terraform-insignia-statefile"
+    dynamodb_table = "insignia-statefile-locking"
+    key            = "ec2-insignia/terraform.tfstate"
+    region         = "us-east-1"
   }
 }
